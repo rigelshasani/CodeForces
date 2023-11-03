4dae044ea0417d46cee11f0218a0d43d755fd12d
Rig <rigels1304@gmail.com>
2023-11-03 18:54:11 -0400

    Finished Beautiful Matrix

---
+++ BeautifulMatrix/main2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    for(int r=1;r < 6;r++){
+        for(int c=1;c<6;c++){
+            cin >> n;
+            if(n == 1){
+                cout << abs(3-r) + abs(3-c);
+            } 
+        }
+    }
+}
