1d01eb468e6aa45386d369c8f394ba4a93d05da5
Rig <rigels1304@gmail.com>
2023-12-18 18:24:50 +0100

    finish perm

--- et20/Permutation/main.cpp
+++ et20/Permutation/main.cpp
@@ -8,4 +8,13 @@ int main(){
     //5
     //1 2 3 4 5
     //3 1 2 5 4
+    if(n % 2 != 0){
+        cout << -1 << "\n";
+    }
+    else{
+        for(int i=1;i<n;i+=2){
+            cout << i+1 << " " << i << " ";
+        }
+        cout << endl;
+    }
 }
