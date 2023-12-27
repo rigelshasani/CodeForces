ea8316f9eb5e566626b002ced9475c8a3641c4f7
Rig <rigels1304@gmail.com>
2023-12-27 10:48:20 +0100

    Finish horseshoe

---
+++ Problem20To30/Horseshoe/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int hoofsNeeded = 0;
+    unordered_set<int> unique;
+    unique.insert(a);
+    unique.insert(b);
+    unique.insert(c);
+    unique.insert(d);
+    
+    if(unique.size() == 4){
+        cout << 0;
+    }
+    else if(unique.size() == 3){
+        cout << 1;
+    }
+    else if(unique.size() == 2){
+        cout << 2;
+    }
+    else{
+        cout << 3;
+    }    
+    //I need to search through the vector and 
+
+
+    // for( int i = 0; i < 4 ; i++ ){
+    //     for(int j = 0; j < 4; i++){
+    //         if( j != i ){
+    //             if(hoofColors[i] == hoofColors[j]){
+    //                 hoofsNeeded++;
+    //             }
+    //         }
+    //     }
+    // }
+}
