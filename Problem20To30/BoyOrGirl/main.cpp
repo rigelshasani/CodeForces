29b6f363512149c35fc430fdcb865020346e3abb
Rig <rigels1304@gmail.com>
2023-12-27 10:53:42 +0100

    Finish boy or girl

---
+++ Problem20To30/BoyOrGirl/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    string username;
+    cin >> username;
+    unordered_set<char> uniqueChars;
+    for( int i=0; i < username.length() ; i++ ){
+        uniqueChars.insert(username[i]);
+    }
+    if(uniqueChars.size() % 2 == 0){
+        cout << "CHAT WITH HER!";
+    }
+    else{
+        cout << "IGNORE HIM!";
+    }
+}
