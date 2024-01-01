e1bc32fe66ef66afe2ea843bbdd67b78cda514f3
Rig <rigels1304@gmail.com>
2024-01-01 11:06:24 +0100

    Name change, added documentation

--- Problem20To30/Lev2EffectiveApproach/main.cpp
+++ Problem20To30/Lev2EffectiveApproach/main.cpp
@@ -7,6 +7,8 @@ int main() {
     int numOfElements; cin >> numOfElements;
     vector<int> positionVector(numOfElements+1);
 
+    //"array" positionVector stores the element as the position and the position as the element
+    //in this way, we can search the element by entering its position....
     for(int i=1; i < numOfElements + 1; i++){
         int num; cin >> num;
         positionVector[num] = i;
@@ -15,6 +17,7 @@ int main() {
     long long vasya = 0, petya = 0;
     int numOfQueries; cin >> numOfQueries;
 
+    //...like here, where vasya is incremented based on the query(search is O(1))
     while(numOfQueries--){
         int query; cin >> query;
         
