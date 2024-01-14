bb18ed2032679abcb19b1f72bc94291fd1a2075b
Rig <rigels1304@gmail.com>
2024-01-13 20:12:50 -0500

    Xenia(-)

--- Problem40To50/XeniaAndRingroad/main.cpp
+++ Problem40To50/XeniaAndRingroad/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int numHouses, numTasks; cin >> numHouses >> numTasks;
     
+    int location = 1;
+    long long int answer = 0;
+    for(int i=0; i<numTasks;i++){
+        int now; cin >> now;
+        if(now >= location) answer += now - location;
+        else{
+            answer += numHouses - (location - now);
+        }
+        location = now;
+    }
+    cout << answer;
 }
