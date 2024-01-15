c4da4b118f179e3d50239a3025d3317f00e6c3fe
Rig <rigels1304@gmail.com>
2024-01-15 18:16:05 -0500

    Magic Numbers

---
+++ Problem40To50/MagicNumbers/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string input;
+    cin >> input;
+    
+    if(input[0] == '4'){
+        cout << "NO";
+        return 0;
+    } 
+    
+    for(int i=0; i < input.length(); i++){
+        if(input[i] != 52 && input[i] != 49){
+            cout << "NO" << endl;
+            return 0;
+        }
+        if(input[i] == '4' && i > 2){
+            if(i != input.length() - 1){
+                if(input[i+1] == 52 && input[i+2] == 52){
+                    cout << "NO" << endl;
+                    return 0;
+                }
+            }
+            else{
+                if(input[i-1] != 49){
+                    if(input[i-2] != 49){
+                        cout << "NO" << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    } 
+    cout << "YES" << endl;
+    return 0;
+}
